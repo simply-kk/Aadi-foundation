8d882c207b0e21e6fd44ccb28f7c4c8fd250c424
Khalid Khan <shhkhan33@gmail.com>
2024-06-19 21:19:03 +0530

    basic use of python

---
+++ python/assignmentArrayPrint.py
@@ -0,0 +1,9 @@
+# function defination for printing each element of array
+def printArray(array):
+    for i in array:
+        print(i)
+
+
+array = [1, 2, 3, 4, 5]
+#function call for printing each element of array:
+printArray(array)
